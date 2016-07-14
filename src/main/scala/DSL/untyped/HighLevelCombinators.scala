1f1394afe6a894f4deec1ea2a2994befd72b629b
Michel Steuwer <michel.steuwer@ed.ac.uk>
2016-07-14 12:05:20 +0100

    Cleanup

--- src/main/scala/DSL/untyped/HighLevelCombinators.scala
+++ src/main/scala/DSL/untyped/HighLevelCombinators.scala
@@ -56,11 +56,7 @@ object iterate {
 }
 
 object gather {
-//  def apply(idxF: Phrase[ExpType -> ExpType]) = λ(array => Gather(null, null, idxF, array))
-
   def apply(idxF: Phrase[`(nat)->`[->[ExpType, ExpType]]]) = {
-    val idxF_ = idxF(NamedVar(newName()))
-    Core.xmlPrinter.toFile("/tmp/s.xml", idxF_)
-    λ(array => Gather(null, null, idxF_, array))
+    λ(array => Gather(null, null, idxF(NamedVar(newName())), array))
   }
 }
