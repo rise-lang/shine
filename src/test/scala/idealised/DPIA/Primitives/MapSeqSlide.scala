37e13546bd53399122e2711bccafb1ec2534efd1
Thomas Koehler <basta.t.k+git@gmail.com>
2019-01-24 15:08:02 +0000

    Check mapSeqSlide expected result

--- src/test/scala/idealised/DPIA/Primitives/MapSeqSlide.scala
+++ src/test/scala/idealised/DPIA/Primitives/MapSeqSlide.scala
@@ -1,18 +1,46 @@
 package idealised.DPIA.Primitives
 
-import idealised.C.Program
 import lift.arithmetic._
 import idealised.SurfaceLanguage.DSL._
 import idealised.SurfaceLanguage.Types._
-import idealised.util.SyntaxChecker
+import idealised.util.{Execute, SyntaxChecker}
 
 class MapSeqSlide extends idealised.util.Tests {
-  test("Simple example should generate syntactic valid C code") {
-    val e = fun(ArrayType(SizeVar("N"), float))(a =>
-      a :>> mapSeqSlide(3, 1, reduceSeq(fun(_ + _), 0.0f)))
+  test("Simple example should generate C code producing the expected result on a test") {
+    val e = fun(ArrayType(SizeVar("N"), int))(a =>
+      a :>> mapSeqSlide(3, 1, reduceSeq(fun(_ + _), 0)))
     val p = idealised.C.ProgramGenerator.makeCode(TypeInference(e, Map()).toPhrase)
     val code = p.code
     SyntaxChecker(code)
-    println(code)
+
+    val testCode = s"""
+#include <stdio.h>
+
+$code
+
+int main(int argc, char** argv) {
+  const int N = 50;
+
+  int input[N];
+  for (int i = 0; i < N; i++) { input[i] = i; }
+
+  int output[N];
+  ${p.function.name}(output, input, N);
+
+  for (int i = 0; i < N-2; i++) {
+    int expected = input[i] + input[i+1] + input[i+2];
+
+    if (output[i] != expected) {
+      fprintf(stderr, "found %i, expected %i\\n", output[i], expected);
+      return 1;
+    }
+  }
+
+  return 0;
+}
+"""
+    Execute(testCode)
+
+    println(testCode)
   }
 }
--- src/test/scala/idealised/util/SyntaxChecker.scala
+++ src/test/scala/idealised/util/SyntaxChecker.scala
@@ -1,6 +1,5 @@
 package idealised.util
 
-import java.io.{ File, PrintWriter }
 import sys.process._
 
 import scala.language.postfixOps
@@ -12,7 +11,7 @@ object SyntaxChecker {
   @throws[SyntaxChecker.Exception]("if code doesn't pass the syntax check")
   def apply(code: String): Unit = {
     try {
-      s"clang -fsyntax-only ${writeToTempFile(code).getAbsolutePath}" !!
+      s"clang -fsyntax-only ${writeToTempFile("code-", ".c", code).getAbsolutePath}" !!
     } catch {
       case _: Throwable =>
         Console.err.println("==========")
@@ -22,18 +21,4 @@ object SyntaxChecker {
         throw Exception(s"Code: `$code' did not pass syntax check")
     }
   }
-
-  private def writeToTempFile(content: String): File = {
-    val tmp = File.createTempFile("code-", ".c")
-    tmp.deleteOnExit()
-    new PrintWriter(tmp) {
-      try {
-        write(content)
-      } finally {
-        close()
-      }
-    }
-    tmp
-  }
-
 }
