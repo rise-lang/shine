9b69b6c777c4875d38ca98e3b76515d4c3cca4f7
Thomas Koehler <basta.t.k+git@gmail.com>
2019-06-24 16:15:18 +0100

    Fixes GEMM inference, stronger nat unification, breaks binomial filter

--- src/test/scala/apps/gemm.scala
+++ src/test/scala/apps/gemm.scala
@@ -57,7 +57,7 @@ class gemm extends idealised.util.Tests {
   val mult  = implT(dt => fun(x => x._1 * x._2) :: ((dt x dt) -> dt))
   val add   = fun(x => fun(y => x + y))
   val scal  = implN(n => fun(xs => fun(a => mapSeq(fun(x => a * x), xs))) :: (ArrayType(n, float) ->: float ->: ArrayType(n, float)))
-  val dot   = fun(xs => fun(ys => zip(xs, ys) |> mapSeq(mult) |> reduceSeq(add, l(0.0f))))
+  val dot = fun(x => foreignFun("dot", float4 ->: float4 ->: float)(x._1, x._2))
 
   val sequential =
     nFun((n, m, k) =>
@@ -83,24 +83,24 @@ class gemm extends idealised.util.Tests {
     val vw: Nat = 4
 
     val zeros = implN(n => implN(m =>
-      generate(fun(IndexType(m))(i => generate(fun(IndexType(n))(j => l(0.0f) )))) ))
+      generate(fun(IndexType(m))(_ => generate(fun(IndexType(n))(_ => l(0.0f) )))) ))
 
     val mali_GEMM =
       nFun((n, m, k) =>
         fun((m`.`k`.`float) ->: (n`.`k`.`float) ->: (m`.`n`.`float) ->: float ->: float ->: (m`.`n`.`float))
         ((a, b, c, alpha, beta) =>
 
-          zip( split(p2, a), split(p2, c) ) |>
+          zip( split(p2)(a), split(p2)(c) ) |>
             mapGlobal(0)(fun(ac =>
-              zip( split(p1, b), split(p1, transpose(ac._2)) ) |>
+              zip( split(p2)(b), split(p1)(transpose(ac._2)) ) |>
                 mapGlobal(1)(fun(bc =>
-                  zip( split(p3, transpose(ac._1)), split(p3, transpose(bc._1)) ) |>
-                    reduceSeq(fun((p67, p236) =>
+                  zip( split(p3)(transpose(ac._1)), split(p3)(transpose(bc._1)) ) |>
+                    reduceSeq(fun((p236, p67) =>
                       zip(p67, transpose(p236._1)) |>
                         mapSeq(fun(p54 =>
-                          zip(p54._1, transpose(p236._1)) |>
+                          zip(p54._1, transpose(p236._2)) |>
                             mapSeq(fun(p157 =>
-                              zip(asVector(vw, p54._2), asVector(vw, p157._2)) |>
+                              zip(asVector(vw)(p54._2), asVector(vw)(p157._2)) |>
                                 mapSeq(fun(x => p157._1 + dot(x)))
                             )) |> join
                         ))
