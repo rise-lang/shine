fbaf21b2887b3ab378e467d74b6f7229f741a005
Bastian Hagedorn <b.hagedorn@wwu.de>
2019-07-12 10:06:27 +0200

    add flatmapfailure

--- src/main/scala/elevate/core/package.scala
+++ src/main/scala/elevate/core/package.scala
@@ -8,8 +8,12 @@ package object core {
   sealed trait RewriteResult {
     def getExprOrElse(e: Expr): Expr
     def get: Expr
+
     def mapSuccess(f: Expr => Expr): RewriteResult
     def flatMapSuccess(f: Expr => RewriteResult): RewriteResult
+
+    def mapFailure(f: Strategy => Strategy): RewriteResult
+    def flatMapFailure(f: Strategy => RewriteResult): RewriteResult
   }
 
   case class Success(e: Expr) extends RewriteResult {
@@ -18,6 +22,9 @@ package object core {
 
     override def mapSuccess(f: Expr => Expr): RewriteResult = Success(f(e))
     override def flatMapSuccess(f: Expr => RewriteResult): RewriteResult = f(e)
+
+    override def mapFailure(f: Strategy => Strategy): RewriteResult = this
+    override def flatMapFailure(f: Strategy => RewriteResult): RewriteResult = this
   }
 
   case class Failure(s: Strategy) extends RewriteResult {
@@ -26,6 +33,9 @@ package object core {
 
     override def mapSuccess(f: Expr => Expr): RewriteResult = this
     override def flatMapSuccess(f: Expr => RewriteResult): RewriteResult = this
+
+    override def mapFailure(f: Strategy => Strategy): RewriteResult = Failure(f(s))
+    override def flatMapFailure(f: Strategy => RewriteResult): RewriteResult = f(s)
   }
 
   case class NotApplicable(s: Strategy) extends Exception
--- src/main/scala/elevate/core/strategies/package.scala
+++ src/main/scala/elevate/core/strategies/package.scala
@@ -1,8 +1,6 @@
 package elevate.core
 
 import lift.core.Expr
-import elevate.core.rules.algorithmic._
-import elevate.core.strategies.algorithmic._
 import strategies.traversal._
 import scala.language.implicitConversions
 
@@ -12,18 +10,10 @@ package object strategies {
     e => Success(e)
 
   def seq: Strategy => Strategy => Strategy =
-    f => s => e => f(e) match {
-      case Success(x) => s(x)
-      case f:Failure => f
-    }
+    f => s => e => f(e).flatMapSuccess(s(_))
 
   def leftChoice: Strategy => Strategy => Strategy =
-    f => s => e => {
-      f(e) match {
-        case s:Success => s
-        case Failure(_) => s(e)
-      }
-    }
+    f => s => e => f(e).flatMapFailure(_ => s(e))
 
   def `try`: Strategy => Strategy =
     s => leftChoice(s)(id)
