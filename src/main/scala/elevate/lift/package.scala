aba15879142b4f10c579d6e3befa0c91d9aa67fd
Bastian Hagedorn <b.hagedorn@wwu.de>
2019-07-15 15:28:50 +0200

    add forgotten files

---
+++ src/main/scala/elevate/lift/package.scala
@@ -0,0 +1,27 @@
+package elevate
+
+import _root_.lift.core._
+import _root_.lift.core.DSL._
+import _root_.lift.core.primitives._
+import elevate.lift.strategies.normalForm._
+
+package object lift {
+
+  def structEq(a: Expr, b: Expr): Boolean = StructuralEquality(
+    reductionNormalForm(a).get, reductionNormalForm(b).get
+  )
+
+  // notation
+  def T: Expr = transpose
+  def S: Expr = split(4)//slide(3)(1)
+  def J: Expr = join
+  def *(x: Expr): Expr = map(x)
+  def **(x: Expr): Expr = map(map(x))
+  def ***(x: Expr): Expr = map(map(map(x)))
+  def ****(x: Expr): Expr = map(map(map(map(x))))
+  def *****(x: Expr): Expr = map(map(map(map(map(x)))))
+  def ******(x: Expr): Expr = map(map(map(map(map(map(x))))))
+  def λ(f: Identifier => Expr): Expr = fun(f)
+
+
+}
