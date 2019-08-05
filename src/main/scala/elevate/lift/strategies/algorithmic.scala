13e9188fa92027d80066f1fe4ec74f70b42aa418
Michel Steuwer <michel.steuwer@glasgow.ac.uk>
2019-08-05 11:42:05 +0100

    Fixed failing tests. mapFirstFission and mapFullFission needs to be written as elevate strategies

--- src/main/scala/elevate/lift/strategies/algorithmic.scala
+++ src/main/scala/elevate/lift/strategies/algorithmic.scala
@@ -1,5 +1,6 @@
 package elevate.lift.strategies
 
+import com.github.ghik.silencer.silent
 import elevate.core.{Failure, Strategy, Success}
 import lift.core.DSL._
 import lift.core._
@@ -11,33 +12,42 @@ object algorithmic {
   // fission of the first function to be applied inside a map
   // *(g >> .. >> f) -> *g >> *(.. >> f)
   def mapFirstFission: Strategy = {
-    case Apply(primitives.map, Lambda(x, Apply(f, y))) =>
-      Success(mapFirstFissionRec(x, fun(e => e), f, y))
+    case Apply(primitives.map, Lambda(x, gx)) =>
+      Success(mapFirstFissionRec(x, fun(e => e), gx))
     case _ => Failure(mapFirstFission)
   }
 
-  @scala.annotation.tailrec
-  private def mapFirstFissionRec(x: Identifier, f: Expr, g: Expr, y: Expr): Expr = {
-    if (y == x) {
-      primitives.map(g) >> primitives.map(f)
-    } else {
-      mapFirstFissionRec(x, fun(e => f(g(e))), g, y)
+  // TODO: this should be expressed with elevate strategies
+  @silent
+  private def mapFirstFissionRec(x: Identifier, f: Expr, gx: Expr): Expr = {
+    gx match {
+      case Apply(f2, gx2) =>
+        if (gx2 == x) {
+          primitives.map(f2) >> primitives.map(f)
+        } else {
+          mapFirstFissionRec(x, fun(e => f(f2(e))), gx2)
+        }
     }
   }
 
   // fission of all the functions chained inside a map
   // *(g >> .. >> f) -> *g >> .. >> *f
   def mapFullFission: Strategy = {
-    case Apply(primitives.map, Lambda(x, Apply(f, y))) =>
-      Success(mapFullFissionRec(x, f, y))
+    case Apply(primitives.map, Lambda(x, gx)) =>
+      Success(mapFullFissionRec(x, gx))
     case _ => Failure(mapFullFission)
   }
 
-  def mapFullFissionRec(x: Identifier, f: Expr, y: Expr): Expr = {
-    if (y == x) {
-      primitives.map(f)
-    } else {
-      mapFullFissionRec(x, f, y) >> primitives.map(f)
+  // TODO: this should be expressed with elevate strategies
+  @silent
+  def mapFullFissionRec(x: Identifier, gx: Expr): Expr = {
+    gx match {
+      case Apply(f, gx2) =>
+        if (gx2 == x) {
+          primitives.map(f)
+        } else {
+          mapFullFissionRec(x, gx2) >> primitives.map(f)
+        }
     }
   }
 }
