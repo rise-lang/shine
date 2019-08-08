528747e2bb50218bea6728f96ed591200b94f8d9
Michel Steuwer <michel.steuwer@glasgow.ac.uk>
2019-08-08 12:32:36 +0100

    Cleanup and added ValueConstants (true, false).

--- src/main/scala/FSmooth/ScalarFunctionConstants.scala
+++ src/main/scala/FSmooth/ScalarFunctionConstants.scala
@@ -6,91 +6,91 @@ import DSL._
 object ScalarFunctionConstants {
   case class `+`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num -> Num )
-    override def copy(t: Type): Constants = new `+`(t)
+    override def copyWithType(t: Type): Constants = new `+`(t)
   }
 
   case class `-`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num -> Num )
-    override def copy(t: Type): Constants = new `-`(t)
+    override def copyWithType(t: Type): Constants = new `-`(t)
   }
 
   case class `*`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num -> Num )
-    override def copy(t: Type): Constants = `*`(t)
+    override def copyWithType(t: Type): Constants = `*`(t)
   }
 
   case class `/`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num -> Num )
-    override def copy(t: Type): Constants = `/`(t)
+    override def copyWithType(t: Type): Constants = `/`(t)
   }
 
   case class `**`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num -> Num )
-    override def copy(t: Type): Constants = `**`(t)
+    override def copyWithType(t: Type): Constants = `**`(t)
   }
 
   case class sign(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num )
-    override def copy(t: Type): Constants = sign(t)
+    override def copyWithType(t: Type): Constants = sign(t)
   }
 
   case class cos(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num )
-    override def copy(t: Type): Constants = cos(t)
+    override def copyWithType(t: Type): Constants = cos(t)
   }
 
   case class tan(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num )
-    override def copy(t: Type): Constants = tan(t)
+    override def copyWithType(t: Type): Constants = tan(t)
   }
 
   case class log(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num )
-    override def copy(t: Type): Constants = log(t)
+    override def copyWithType(t: Type): Constants = log(t)
   }
 
   case class exp(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num )
-    override def copy(t: Type): Constants = exp(t)
+    override def copyWithType(t: Type): Constants = exp(t)
   }
 
   case class sqrt(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => Num -> Num )
-    override def copy(t: Type): Constants = sqrt(t)
+    override def copyWithType(t: Type): Constants = sqrt(t)
   }
 
   case class `>`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => (Num -> Num) -> Bool )
-    override def copy(t: Type): Constants = `>`(t)
+    override def copyWithType(t: Type): Constants = `>`(t)
   }
 
   case class `<`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => (Num -> Num) -> Bool )
-    override def copy(t: Type): Constants = `<`(t)
+    override def copyWithType(t: Type): Constants = `<`(t)
   }
 
   case class `=:=`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => (Num -> Num) -> Bool )
-    override def copy(t: Type): Constants = `=:=`(t)
+    override def copyWithType(t: Type): Constants = `=:=`(t)
   }
 
   case class `<>`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => (Num -> Num) -> Bool )
-    override def copy(t: Type): Constants = `<>`(t)
+    override def copyWithType(t: Type): Constants = `<>`(t)
   }
 
   case class `&&`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => (Num -> Num) -> Bool )
-    override def copy(t: Type): Constants = `&&`(t)
+    override def copyWithType(t: Type): Constants = `&&`(t)
   }
 
   case class `||`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implNum(Num => (Num -> Num) -> Bool )
-    override def copy(t: Type): Constants = `||`(t)
+    override def copyWithType(t: Type): Constants = `||`(t)
   }
 
   case class `!`(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = Bool -> Bool
-    override def copy(t: Type): Constants = new `!`(t)
+    override def copyWithType(t: Type): Constants = new `!`(t)
   }
 }
--- src/main/scala/FSmooth/VectorFunctionConstants.scala
+++ src/main/scala/FSmooth/VectorFunctionConstants.scala
@@ -5,21 +5,21 @@ import DSL._
 object VectorFunctionConstants {
   case class build(override val t: Type) extends Constants(t) {
      override def typeScheme: Type = implM(M => Card -> (Index -> M) -> Array(M))
-    override def copy(t: Type): Constants = build(t)
+    override def copyWithType(t: Type): Constants = build(t)
   }
 
   case class ifold(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implM(M => (M -> Index -> M) -> M -> Card -> M)
-    override def copy(t: Type): Constants = ifold(t)
+    override def copyWithType(t: Type): Constants = ifold(t)
   }
 
   case class get(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implM(M => Array(M) -> Index -> M)
-    override def copy(t: Type): Constants = get(t)
+    override def copyWithType(t: Type): Constants = get(t)
   }
 
   case class length(override val t: Type) extends Constants(t) {
     override def typeScheme: Type = implM(M => Array(M) -> Card)
-    override def copy(t: Type): Constants = length(t)
+    override def copyWithType(t: Type): Constants = length(t)
   }
 }
