32374086537783ad664edb2b77bc2cedd86f44c1
Michel Steuwer <michel.steuwer@glasgow.ac.uk>
2019-08-09 17:02:58 +0100

    Some progress

--- src/main/scala/FSmooth/Differentiation.scala
+++ src/main/scala/FSmooth/Differentiation.scala
@@ -23,28 +23,60 @@ object Differentiation {
     }
   }
 
-  def D(e: Expr): Expr = ???
+  def D(e: Expr): Expr = e match {
+    case Application(VectorFunctionConstants.build(_), Seq(e0, e1), _) =>
+      build( fst(D(e0)), fun(i => D(e1) (pair(i, scalar(0)))) )
+    case Application(VectorFunctionConstants.ifold(_), Seq(e0, e1, e2), _) =>
+      ifold( fun( (x, i) => D(e0) (x, pair(i, scalar(0)))), D(e1), fst(D(e2)) )
+    case Application(VectorFunctionConstants.get(_), Seq(e0, e1), _) =>
+      D(e0).get(fst(D(e1)))
+    case Application(VectorFunctionConstants.length(_), Seq(e0), _) =>
+      pair(len(D(e0)), scalar(0))
+    case Application(PairFunctionConstants.pair(_), Seq(e0, e1), _) =>
+      pair(D(e0), D(e1))
+    case Application(PairFunctionConstants.fst(_), Seq(e0), _) =>
+      fst(D(e0))
+    case Application(PairFunctionConstants.snd(_), Seq(e0), _) =>
+      snd(D(e0))
+    case Application(e0, Seq(e1), _)  => D(e0) (D(e1))
+    case Abstraction(Seq(x), e, _)    => fun(Seq(mark(x)) -> D(e))
+    case v: Variable                  => mark(v)
+    case Let(x, e1, e2, _)            => Let(mark(x), D(e1), D(e2))
+    case Conditional(e1, e2, e3, _)   =>`if` (fst(D(e1))) `then` D(e2) `else` D(e3)
+    case ScalarValue(e)               => pair(P(e), E(e))
+  }
+
+  def P(e: Double) = ???
+  def E(e: Double) = ???
+
+  def mark(v: Variable): Variable = {
+    v.copy(v.name + "_")
+  }
 
   // free variables
   def fvs(expr: Expr): Seq[Variable] = {
     val free = mutable.Buffer[Variable]()
-    case class Visitor(bound: Set[Variable]) extends traversal.Visitor {
-      override def apply(e: Expr): traversal.Result[Expr] = {
-        e match {
-          case Abstraction(params, _, _) =>
-            Continue(e, Visitor(bound ++ params))
-          case Let(x, value, body, _) =>
-            apply(value) // visit value
-            traversal.DepthFirstLocalResult(body, Visitor(bound ++ Set(x))) // visit body
-            Stop(e)
-          case v: Variable =>
-            if (!bound.contains(v)) { free += v }
-            Stop(v)
-          case _ => Continue(e, this)
-        }
-      }
+
+    def visit(e: Expr, bound: Set[Variable]): Unit = e match {
+      case Abstraction(params, body, _) =>
+        visit(body, bound ++ params)
+      case Application(fun, args, _) =>
+        visit(fun, bound)
+        args.foreach(visit(_, bound))
+      case Let(x, value, body, _) =>
+        visit(value, bound)
+        visit(body, bound + x)
+      case Conditional(c, tr, el, _) =>
+        visit(c, bound)
+        visit(tr, bound)
+        visit(el, bound)
+      case v: Variable =>
+        if (!bound.contains(v)) { free += v }
+      case ScalarValue(_) | IndexValue(_) |
+           CardinalityValue(_) | _: Constants =>
     }
-    traversal.DepthFirstLocalResult(expr, Visitor(Set()))
+
+    visit(expr, Set())
     free
   }
 
