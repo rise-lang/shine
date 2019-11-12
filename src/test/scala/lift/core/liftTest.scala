46a64174e847cdcf6666baf83734b298a87fbc10
Rongxiao Fu <2109685F@student.gla.ac.uk>
2019-11-12 12:01:49 +0000

    rollback to old toString implementation

--- src/test/scala/lift/core/liftTest.scala
+++ src/test/scala/lift/core/liftTest.scala
@@ -5,8 +5,6 @@ import lift.core.HighLevelConstructs._
 import lift.core.primitives._
 import lift.core.semantics.Data
 import lift.core.types._
-import lift.core.DrawAST._
-import lift.core.DrawTree._
 import apps.convolution._
 
 class liftTest extends test_util.Tests {
