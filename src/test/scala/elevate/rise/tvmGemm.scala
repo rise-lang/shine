9079487f61c9a36010264b9c55819282afdc98ba
Bastian Hagedorn <b.hagedorn@wwu.de>
2020-02-13 10:50:00 +0100

    WIP re-implement liftReduce versions

--- src/test/scala/elevate/rise/tvmGemm.scala
+++ src/test/scala/elevate/rise/tvmGemm.scala
@@ -8,18 +8,19 @@ import elevate.rise.rules.algorithmic._
 import elevate.rise.rules.lowering.lowerToC
 import elevate.rise.rules.movement._
 import elevate.rise.strategies.tiling._
-import elevate.rise.strategies.normalForm.{LCNF, RNF}
+import elevate.rise.strategies.normalForm._
+import elevate.test_util
 import rise.core.dotPrinter._
 import rise.core.TypedDSL._
-import rise.core.types.{ArrayType, float, infer}
+import rise.core.types.{ArrayType, f32, infer}
 import util.gen
 
 class tvmGemm extends test_util.Tests {
 
   val N = 2048 // todo change later to 1024
   val mm = infer(
-    fun(ArrayType(N, ArrayType(N, float)))(a =>
-      fun(ArrayType(N, ArrayType(N, float)))(b =>
+    fun(ArrayType(N, ArrayType(N, f32)))(a =>
+      fun(ArrayType(N, ArrayType(N, f32)))(b =>
         map(fun(ak =>
           map(fun(bk =>
             (reduce(add)(l(0.0f)) o
@@ -30,17 +31,20 @@ class tvmGemm extends test_util.Tests {
   // todo remove before PR
   def toDot(n: String): Strategy[Rise] = peek(x => exprToDot(n, x))
 
+  // utils
+  def currentTimeSec: Long = System.currentTimeMillis / 1000
+
   // ***************************************************************************
 
   test("baseline") {
-    val time0 = System.currentTimeMillis / 1000
+    val time0 = currentTimeSec
     val result = (LCNF `;` oncetd.apply(fuseReduceMap) `;` lowerToC)(mm)
-    val time1 = System.currentTimeMillis / 1000
+    val time1 = currentTimeSec
     println(time1 - time0)
 
-    val time2 = System.currentTimeMillis / 1000
+    val time2 = currentTimeSec
     println(gen.CProgram(result))
-    val time3 = System.currentTimeMillis / 1000
+    val time3 = currentTimeSec
     println(time3 - time2)
   }
 
@@ -50,27 +54,28 @@ class tvmGemm extends test_util.Tests {
   val blocking = // general strategy: tile all loops first then reorder
   // prepare tiling, keep reduce(+) and map(*) close together, (necessary?)
     LCNF `;` oncetd(fuseReduceMap) `;`
-      // M.N.m.n.K (or in TVM terms: xo,yo,xi,yi,k)
-      oncetd(tileNDList(List(32,32))) `;` LCNF `;`
-      // M.N.m.n.K.k (tile K-loop),
-      // fission first to enable blocking the reduction loop
-      oncetd(fissionReduceMap) `;` oncetd(blockedReduce(4)) `;` LCNF `;`
-      // move the split (blocking the reduction loop)
-      // to prepare fusing map(*) and reduce(+) again
-      oncetd(mapFBeforeSlide) `;` LCNF `;`
-      // move map(*) into both reduce loops again
-      oncetd(fuseReduceMap) `;` LCNF `;` oncetd(fuseReduceMap) `;` LCNF `;`
-      // move outer K loop further up M.N.K.m.n.k
-      RNF `;` oncetd(liftReduceOld) `;` LCNF `;`
-      RNF `;` oncetd(liftReduceOld) `;` LCNF `;` toDot("left") `;`
-      // move inner K loop further up M.N.K.k.m.n, inner reduction is now part of the outer-reduction operator
-      oncetd(liftReduceInReduceOperator)// `;` LCNF `;`
-      oncetd(liftReduceInReduceOperator)
+    // M.N.m.n.K (or in TVM terms: xo,yo,xi,yi,k)
+    oncetd(tileNDList(List(32,32))) `;` LCNF `;`
+    // M.N.m.n.K.k (tile K-loop),
+    // fission first to enable blocking the reduction loop
+    oncetd(fissionReduceMap) `;` oncetd(blockedReduce(4)) `;` LCNF `;`
+    // move the split (blocking the reduction loop)
+    // to prepare fusing map(*) and reduce(+) again
+    oncetd(mapFBeforeSlide) `;` LCNF `;`
+    // move map(*) into both reduce loops again
+    oncetd(fuseReduceMap) `;` LCNF `;` oncetd(fuseReduceMap) `;` LCNF `;`
+    // move outer K loop further up M.N.K.m.n.k
+    RNF `;` oncetd(liftReduce) `;` LCNF `;` RNF `;` toDot("left") //`;`
+    RNF `;` oncetd(liftReduce) `;` LCNF `;` RNF //`;`
+    // move inner K loop further up M.N.K.k.m.n,
+    // inner reduction is now part of the outer-reduction operator
+    oncetd(liftReduceInReduceOperator2) `;` LCNF `;` RNF `;` toDot("left") `;`
+    oncebu(liftReduce)
 
   test("blocking") {
-    val result = (blocking `;` lowerToC)(mm)
+    val result = (blocking `;` lowerToC `;` toDot("right"))(mm)
     println(result)
-    gen.CProgram(result)
+    gen.CProgram(infer(result))
   }
 
 }
