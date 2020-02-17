9a368e209f0c6c72d158c492f3c5685c371c0fcf
Bastian Hagedorn <b.hagedorn@wwu.de>
2020-02-17 10:28:43 +0100

    be more specific about paper

--- src/main/scala/rise/core/dotPrinter.scala
+++ src/main/scala/rise/core/dotPrinter.scala
@@ -49,7 +49,7 @@ case object dotPrinter {
           this.copy(decorations =
             x => s"<font color='gray'>${decorations(x)}</font>"
           )
-        // keep for now: used as style in the paper
+        // keep for now: used as style in the arXive paper
         //def green: Label =
         //  this.copy(decorations =
         //    x => s"""<font color="#3C8031">${decorations(x)}</font>"""
