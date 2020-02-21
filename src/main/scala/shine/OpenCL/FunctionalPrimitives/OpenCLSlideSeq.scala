ce40e253c0ccde0361ae4f8d77de25ef7bac0497
Thomas Koehler <basta.t.k+git@gmail.com>
2020-02-21 14:32:15 +0000

    some style fixes

--- src/main/scala/shine/OpenCL/FunctionalPrimitives/OpenCLSlideSeq.scala
+++ src/main/scala/shine/OpenCL/FunctionalPrimitives/OpenCLSlideSeq.scala
@@ -48,10 +48,12 @@ final case class OpenCLSlideSeq(rot: lp.SlideSeq.Rotate,
   }
 
   override def acceptorTranslation(A: Phrase[AccType])
-                                  (implicit context: TranslationContext)
+    (implicit context: TranslationContext)
   : Phrase[CommType] = {
     import TranslationToImperative._
-    import shine.OpenCL.IntermediatePrimitives.{OpenCLSlideSeqIValues, OpenCLSlideSeqIIndices}
+    import shine.OpenCL.IntermediatePrimitives.{
+      OpenCLSlideSeqIValues, OpenCLSlideSeqIIndices
+    }
 
     val I = rot match {
       case lp.SlideSeq.Values => OpenCLSlideSeqIValues.apply _
