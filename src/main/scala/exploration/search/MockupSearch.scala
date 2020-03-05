83e913ccb3c7e28301860ef3e99a178be31a2c5a
Johannes Lenfers <j.le@wwu.de>
2020-03-05 14:53:30 +0100

    added blocking strategy to heuristic exploration

--- src/main/scala/exploration/search/MockupSearch.scala
+++ src/main/scala/exploration/search/MockupSearch.scala
@@ -1,26 +1,69 @@
 package exploration.search
 
-import elevate.core.strategies.traversal.{alltd, bottomup, oncetd, topdown}
-import elevate.core.{Strategy, Success}
+import elevate.core.strategies.traversal.{alltd, bottomup, oncebu, oncetd, topdown}
+import elevate.core.{Failure, Strategy, Success}
 import elevate.heuristic_search.ProblemConstraints
-import elevate.rise.rules.lowering.{mapSeq, reduceSeq}
-import elevate.rise.{Rise, rules, strategies}
+import elevate.rise.rules.algorithmic.{blockedReduce, fissionReduceMap, fuseReduceMap}
+import elevate.rise.rules.movement.{liftReduce, mapFBeforeSlide}
+import elevate.rise.{Rise, rules}
 import elevate.rise.rules.traversal.LiftTraversable
-import elevate.rise.strategies.normalForm.{CNF, RNF}
-import elevate.rise.strategies.{tiling, traversal}
+import elevate.rise.strategies.normalForm.{LCNF, RNF}
+import elevate.rise.strategies.tiling
+import elevate.rise.strategies.tiling.tileNDList
 
-//simple mockup search class
-class MockupSearch(val lowering: Strategy[Rise] = CNF `;` alltd(reduceSeq) `;` alltd(mapSeq))
+import scala.collection.mutable
+
+// simple mockup search class
+class MockupSearch(val lowering: Strategy[Rise] = elevate.rise.rules.lowering.lowerToC)
   extends ProblemConstraints[Rise] {
 
-  var value = 12
+  // blocking strategy: to be moved!
+
+  // prepare tiling, keep reduce(+) and map(*) close together, (necessary?)
+  val fusedReduceMap: Strategy[Rise] = LCNF `;` oncetd(fuseReduceMap)
+
+  // M.N.m.n.K (or in TVM terms: xo,yo,xi,yi,k)
+  val tiledOuterTwo: Strategy[Rise] = fusedReduceMap `;`
+    oncetd(tileNDList(List(32,32))) `;` LCNF
+
+  // M.N.m.n.K.k (tile K-loop),
+  // fission first to enable blocking the reduction loop
+  val splitK: Strategy[Rise] = tiledOuterTwo `;`
+    oncetd(fissionReduceMap) `;` oncetd(blockedReduce(4)) `;` LCNF
+
+  // move the split (blocking the reduction loop)
+  // to prepare fusing map(*) and reduce(+) again
+  val prepareFusion: Strategy[Rise] = splitK `;`
+    oncetd(mapFBeforeSlide) `;` LCNF
+
+  // move map(*) into both reduce loops again
+  val fusedReduceMapAgain: Strategy[Rise] = prepareFusion `;`
+    oncetd(fuseReduceMap) `;` LCNF `;` oncetd(fuseReduceMap) `;` LCNF
+
+  // move outer K loop up M.N.m.K.n.k
+  val moveOuterKLoopOnce: Strategy[Rise] = fusedReduceMapAgain `;`
+    RNF `;` oncetd(liftReduce) `;` LCNF
+
+  // move outer K loop further up M.N.K.m.n.k
+  val moveOuterKLoopTwice: Strategy[Rise] = moveOuterKLoopOnce `;`
+    RNF `;` oncetd(liftReduce) `;` LCNF
+
+  // move inner K loop further up M.N.K.m.k.n,
+  val moveInnerKLoopOnce: Strategy[Rise] = moveOuterKLoopTwice `;`
+    RNF `;` oncebu(liftReduce) `;` LCNF
+
+  val blocking : Strategy[Rise] = moveInnerKLoopOnce `;`
+    RNF `;` oncebu(liftReduce)
+
   val traversalss = Seq(
     elevate.core.strategies.traversal.oncetd,
     elevate.core.strategies.traversal.alltd,
     elevate.core.strategies.traversal.topdown,
     elevate.core.strategies.traversal.bottomup
   )
+
   val strategies = Seq(
+    blocking,
     rules.algorithmic.splitJoin(8),
     rules.algorithmic.mapLastFission,
     rules.algorithmic.mapFusion,
@@ -40,12 +83,12 @@ class MockupSearch(val lowering: Strategy[Rise] = CNF `;` alltd(reduceSeq) `;` a
     rules.movement.transposeBeforeMapMapF,
     rules.movement.transposeBeforeMapJoin,
     tiling.loopInterchange,
-//    tiling.tileND(32)(32),
+    tiling.tileND(32)(32),
 //
-    rules.betaReduction,
-    rules.etaAbstraction,
-    rules.etaReduction
-
+//    rules.betaReduction,
+//    rules.etaAbstraction,
+//    rules.etaReduction
+//
 //  rules.inferRise,
 //  rules.traversal.LiftTraversable,
 //  rules.traversal.argument,
@@ -54,45 +97,47 @@ class MockupSearch(val lowering: Strategy[Rise] = CNF `;` alltd(reduceSeq) `;` a
 //  rules.traversal.function
   )
 
+  val solutions = new mutable.HashMap[Int, Double]()
+
   def N(solution:Rise):Set[Rise] = {
     val neighbours = scala.collection.mutable.Set[Rise]()
 
     //try strategies and add to neighbourhood set
     strategies.foreach(strategy  => {
         try {
-          print("try: " + strategy)
-          val resultOncetd = (RNF `;` oncetd(strategy))(solution)
-          val resultAlltd = (RNF `;` alltd(strategy))(solution)
-          val resultTopdown = (RNF `;` topdown(strategy))(solution)
-          val resultBottomUp = (RNF `;` bottomup(strategy))(solution)
+//          print("try: " + strategy)
+          val resultOncetd = oncetd(strategy).apply(solution)
+          val resultAlltd = alltd(strategy).apply(solution)
+          val resultTopdown = topdown(strategy).apply(solution)
+          val resultBottomUp = bottomup(strategy).apply(solution)
 
           //check success/failure
           if (resultOncetd.isInstanceOf[Success[Rise]]) {
-            println(" - success")
+//            println(" - success")
             neighbours.add(resultOncetd.get)
           }else{
-            println(" - failure")
+//            println(" - failure")
           }
-          //check success/failure
+//          //check success/failure
           if (resultAlltd.isInstanceOf[Success[Rise]]) {
-            println(" - success")
+//            println(" - success")
             neighbours.add(resultAlltd.get)
           }else{
-            println(" - failure")
+//            println(" - failure")
           }
           //check success/failure
           if (resultTopdown.isInstanceOf[Success[Rise]]) {
-            println(" - success")
+//            println(" - success")
             neighbours.add(resultTopdown.get)
           }else{
-            println(" - failure")
+//            println(" - failure")
           }
           //check success/failure
           if (resultBottomUp.isInstanceOf[Success[Rise]]) {
-            println(" - success")
+//            println(" - success")
             neighbours.add(resultBottomUp.get)
           }else{
-            println(" - failure")
+//            println(" - failure")
           }
         }catch{
           case e:Throwable => {
@@ -107,20 +152,24 @@ class MockupSearch(val lowering: Strategy[Rise] = CNF `;` alltd(reduceSeq) `;` a
     neighbours.toSet
   }
 
+  // warning: check size of hashmap
   def f(solution:Rise):Double = {
-    println("lowering strategy: " + lowering)
     try {
-      //codegen normal form (CNF)
-      val cnf = CNF.apply(solution)
+      if(solutions.get(solution.hashCode()).isDefined){
+        //get elements from hashmap and return buffered runtime
+        solutions.get(solution.hashCode()).get
+      }else {
+        //lower
+        val lowered = lowering(solution)
 
-      //lower CNF
-      var lowered = alltd(mapSeq).apply(cnf)
-      lowered = alltd(reduceSeq).apply(lowered)
+        //execute
+        val performanceValue = executeC(lowered)
 
-      //execute
-      val performanceValue = executeC(lowered)
-      performanceValue
+        //add to hashmap
+        solutions.+=(solution.hashCode() -> performanceValue)
 
+        performanceValue
+      }
     }catch{
       case e: Throwable => {
         println("error: " + e)
--- src/test/scala/exploration/explore.scala
+++ src/test/scala/exploration/explore.scala
@@ -1,11 +1,17 @@
 package exploration
 
+import elevate.core.Strategy
 import rise.core.TypedDSL.{add, fst, fun, l, map, reduce, snd, transpose, zip}
 import rise.core.types.{ArrayType, f32, infer}
-import elevate.core.strategies.traversal.{alltd}
+import elevate.core.strategies.traversal.{alltd, oncebu, oncetd}
+import elevate.rise.Rise
+import elevate.rise.rules.algorithmic.{blockedReduce, fissionReduceMap, fuseReduceMap}
 import elevate.rise.rules.lowering.{mapSeq, reduceSeq}
+import elevate.rise.rules.movement.{liftReduce, mapFBeforeSlide}
 import elevate.rise.rules.traversal.LiftTraversable
-import elevate.rise.strategies.normalForm.CNF
+import elevate.rise.strategies.normalForm.{CNF, LCNF, RNF}
+import elevate.rise.strategies.tiling.tileNDList
+import util.gen
 
 
 
@@ -24,13 +30,12 @@ class explore extends shine.test_util.Tests {
   )
 
   test("test exploration"){
-    val lowering = CNF `;` alltd(reduceSeq) `;` alltd(mapSeq)
+    val lowering = elevate.rise.rules.lowering.lowerToC
 
     val result = riseExploration(mm, lowering)
 
     println("result: " + result)
 
   }
-
 
 }
