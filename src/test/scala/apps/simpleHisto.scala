c2db44f684809eb4df957daa65c1a5462cc6c964
Köpcke <bastian.koepcke@wwu.de>
2020-08-31 17:18:50 +0200

    first outline for a functional If primitive

--- src/test/scala/apps/simpleHisto.scala
+++ src/test/scala/apps/simpleHisto.scala
@@ -16,23 +16,41 @@ class simpleHisto extends shine.test_util.TestsWithExecutor {
   def id: Expr = fun(x => x)
 
   test("Simple Histogram Test") {
-    val k: Nat = 4
+    val numBins: Nat = 4
 
-    val simpleHistoTest = nFun(n => fun(xsT(n))(is =>
-      is |>
-      split(1) |>
+    def incrementBin(
+      numBins: Int,
+      input: Expr,
+      hist: Identifier,
+      binCounter: Int = 0
+    ): Expr = {
+      // If (primitive):
+      // exp[t, read] -> exp[bool, read] ->
+      //  (exp[t, read] -> exp[s, write]) ->
+      //  (exp[t, read] -> exp[s, write]) -> exp[s, write]
+
+      //If(input, (input == l(binCounter)),
+      // Set(hist, idx(binCounter, numBins), (idx(a, lidx(binCounter, n)))),
+      // incrementBin(numBins, input, hist, ++binCounter))
+    }
+
+    val simpleHistoTest = nFun(n => nFun(chunkSize => fun(xsT(n))(is =>
+      is |> // n.int
+      split(chunkSize) |> //n/chunkSize.chunkSize.int
       mapGlobal(
         oclReduceSeq(AddressSpace.Private)(
-          fun(a => fun(i =>
-            a |>
-            mapSeq(fun(x => x))
+          fun(a => // numBins.int
+            fun(i => // int
+              // if (i == 0) set(a, lidx(0, numBins), a[lidx(0, numBins)]+1)) else
+              //   if (i == 1) set(a, lidx(1, numBins), a[lidx(1, numBins)]+1)) else
+              //     if (i == 2) set(a, lidx(2, numBins), a[lidx(2, numBins)]+1))
           ))
         )(
-          generate(fun(IndexType(k))(_ => l(0))) |>
-          mapSeq(fun(x => x))
+          generate(fun(IndexType(numBins))(_ => l(0))) |> // numBins.int, read
+          mapSeq(fun(x => x)) // numBins.int, write
         )
       )
-    ))
+    )))
 
     gen.OpenCLKernel(simpleHistoTest)
   }
