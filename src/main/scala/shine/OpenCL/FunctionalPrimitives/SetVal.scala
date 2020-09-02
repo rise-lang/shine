06cee272548e760443a71bd2c95100c711b4031e
Tom Deuschle <tom-d.172@hotmail.de>
2020-09-02 16:08:52 +0200

    Renamed Set to SetVal + first additions to CodeGenerator

--- src/main/scala/shine/OpenCL/FunctionalPrimitives/SetVal.scala
+++ src/main/scala/shine/OpenCL/FunctionalPrimitives/SetVal.scala
@@ -10,13 +10,13 @@ import shine.DPIA._
 
 import scala.xml.Elem
 
-// set : (n : nat) -> (t : data) -> exp[n.t, read] -> exp[idx[n], read] -> exp[t, read] -> exp[n.t, read]
+// setVal : (n : nat) -> (t : data) -> exp[n.t, read] -> exp[idx[n], read] -> exp[t, read] -> exp[n.t, read]
 
-final case class Set(n: Nat,
-                     dt: DataType,
-                     array: Phrase[ExpType],
-                     index: Phrase[ExpType],
-                     value: Phrase[ExpType])
+final case class SetVal(n: Nat,
+                        dt: DataType,
+                        array: Phrase[ExpType],
+                        index: Phrase[ExpType],
+                        value: Phrase[ExpType])
   extends ExpPrimitive {
 
   array :: expT(n`.`dt, read)
@@ -26,14 +26,13 @@ final case class Set(n: Nat,
 
   override def eval(s: Store): Data = ???
 
-  override def visitAndRebuild(
-                                fun: VisitAndRebuild.Visitor
+  override def visitAndRebuild(fun: VisitAndRebuild.Visitor
                               ): Phrase[ExpType] = {
-    Set(fun.nat(n),
-        fun.data(dt),
-        VisitAndRebuild(array, fun),
-        VisitAndRebuild(index, fun),
-        VisitAndRebuild(value, fun)
+    SetVal(fun.nat(n),
+           fun.data(dt),
+           VisitAndRebuild(array, fun),
+           VisitAndRebuild(index, fun),
+           VisitAndRebuild(value, fun)
     )
   }
 
@@ -53,19 +52,20 @@ final case class Set(n: Nat,
     con(array)(λ(expT(n`.`dt, read))(e =>
       con(index)(fun(index.t)(i =>
         con(value)(λ(expT(dt, read))(x =>
-        C(Set(n, dt, i, e, x))))))))
+          C(SetVal(n, dt, e, i, x))))))))
   }
 }
 
-object Set {
-  def apply(array: Phrase[ExpType], index: Phrase[ExpType], value: Phrase[ExpType]): Set = {
+/*
+object SetVal {
+  def apply(array: Phrase[ExpType], index: Phrase[ExpType], value: Phrase[ExpType]): SetVal = {
     (array.t, index.t, value.t) match {
       case (ExpType(ArrayType(n1, dt1), _: read.type),
         ExpType(IndexType(n2), _: read.type),
         ExpType(dt2, _: read.type)
       ) if (n1 == n2) && (dt1 == dt2) =>
-        Set(n1, dt1, array, index, value)
+        SetVal(n1, dt1, array, index, value)
       case x => error(x.toString, "(exp[n.dt], exp[idx(n)], exp[dt])")
     }
   }
-}
+}*/
