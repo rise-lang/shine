92b03b66b59249bd05e71064008db1cd253ad614
Federico Pizzuti <federicopizzuti@Federicos-MacBook-Pro.local>
2020-09-22 14:15:25 +0100

    Comment suggestion

--- src/main/scala/rise/core/types/Flags.scala
+++ src/main/scala/rise/core/types/Flags.scala
@@ -4,7 +4,7 @@ object Flags {
   /** This flag enables the type inference system to express explicit dependence
     * of type variables within dependent functions.
     *
-    * The pass is slow and interferes with advanced arithmetic simplification.
+    * The transformation is slow and interferes with advanced arithmetic simplification.
     * However, it is necessary to use Dependent Arrays properly.
     * */
   sealed abstract class ExplicitDependence
