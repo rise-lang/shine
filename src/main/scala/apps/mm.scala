887e539bef472a31a7e90c6584039201f03fbfaa
Johannes Lenfers <j.le@wwu.de>
2020-10-23 01:47:44 +0200

    get parameters for nvidia mm as args from hypermapper and return runtime

--- src/main/scala/apps/mm.scala
+++ src/main/scala/apps/mm.scala
@@ -122,6 +122,66 @@ object mm {
     ))
   }
 
+  def genMMKernel(v3:Int, v4:Int, v5:Int, v6:Int, v7:Int, v8:Int):Expr = {
+    nFun((n, m, o) => fun(
+      (o`.`n`.`f32) ->: (o`.`m`.`f32) ->: (n`.`m`.`f32)
+    )((at, b) =>
+      at |>
+        map(split(v5)) |> split(v8) |> // O'.v8.N'.v5.f
+        map(transpose) |> transpose |> // N'.O'.v8.v5.f
+        mapWorkGroup(1)(fun(p2 =>
+          b |>
+            map(split(v7)) |> split(v8) |> // O'.v8.M'.v7.f
+            map(transpose) |> transpose |> // M'.O'.v8.v7.f
+            mapWorkGroup(0)(fun(p3 =>
+              zip(p2, p3) |> // O'.(v8.v5.f x v8.v7.f)
+                oclReduceSeq(AddressSpace.Local)(fun((p13, p14) =>
+                  // (v5/^v4).(v7/^v3).v4.v3.f x (v8.v5.f x v8.v7.f)
+                  let (toLocal(pair(
+                    p14._1 |> join |> split(v6) |> // ((v8 x v5) /^ v6).v6.f
+                      mapLocal(1)(asScalar o mapLocal(0)(id) o asVectorAligned(4)) |>
+                      join |> split(v5), // v8.v5.f
+                    p14._2 |> // v8.v7.f
+                      mapLocal(1)(asScalar o mapLocal(0)(id) o asVectorAligned(4))
+                  )))
+                    be(p15 =>
+                    zip(p13, split(v4)(transpose(p15._1))) |> // (v5/^v4).((v7/^v3).v4.v3.f x v4.v8.f)
+                      mapLocal(1)(fun(p16 =>
+                        zip(p16._1, split(v3)(transpose(p15._2))) |> // (v7/^v3).(v4.v3.f x v3.v8.f)
+                          mapLocal(0)(fun(p17 =>
+                            zip(transpose(p16._2), transpose(p17._2)) |> // v8.(v4.f x v3.f)
+                              oclReduceSeq(AddressSpace.Private)(fun((p19, p20) =>
+                                // v4.v3.f x (v4.f x v3.f)
+                                let (toPrivate(pair(mapSeq(id)(p20._1), mapSeq(id)(p20._2))))
+                                  be (p21 =>
+                                  zip(p19, p21._1) |> // v4.(v3.f x f)
+                                    mapSeq(fun(p22 =>
+                                      zip(p22._1, p21._2) |> // v3.(f x f)
+                                        mapSeq(fun(p23 =>
+                                          p23._1 + (p22._2 * p23._2)
+                                        ))
+                                    ))
+                                  )
+                              ))(p17._1 // v4.v3.f
+                                |> mapSeq(mapSeq(id)) // TODO: think about that
+                              ) |> mapSeq(mapSeq(id)) // TODO: think about that
+                          ))
+                      ))
+                    )
+                ))(
+                  generate(fun(_ =>
+                    generate(fun( _ =>
+                      generate(fun(_ =>
+                        generate(fun(_ => l(0.0f))))))))) |>
+                    mapLocal(1)(mapLocal(0)(mapSeq(mapSeq(id))))
+                ) |> // (v5/^v4).(v7/^v3).v4.v3.f
+                mapLocal(1)(mapLocal(0)(mapSeq(asScalar o mapSeq(id) o asVector(4)))) |>
+                map(transpose) |> join |> map(join) |> transpose // v7.v5.f
+            )) |> join |> transpose // v5.M.f
+        )) |> join // N.M.f
+    ))
+  }
+
   def computeGold(n: Int, m: Int, o: Int,
                   At: Array[Array[Float]],
                   B: Array[Array[Float]]): Array[Array[Float]] = {
---
+++ src/main/scala/apps/mmTuning.scala
@@ -0,0 +1,68 @@
+package apps
+
+import mm._
+import opencl.executor.Executor
+import shine.OpenCL.{GlobalSize, LocalSize}
+import util.gen
+
+object mmTuning {
+
+  // we don't tune input size
+  private val N = 64
+  private val M = 128
+  private val O = 128
+
+  private def randGold(): (Array[Array[Float]], Array[Array[Float]], Array[Float]) = {
+    val rand = new scala.util.Random
+    val At = Array.fill(O, N)(rand.nextFloat() * 10)
+    val B = Array.fill(O, M)(rand.nextFloat() * 10)
+    val gold = computeGold(N, M, O, At, B).flatten
+    (At, B, gold)
+  }
+
+  // main
+  def main(args: Array[String]): Unit = {
+    Executor.loadLibrary()
+    Executor.init()
+
+    // read in values from args
+    val v3 = args(0).toInt
+    val v4 = args(1).toInt
+    val v5 = args(2).toInt
+    val v6 = args(3).toInt
+    val v7 = args(4).toInt
+    val v8 = args(5).toInt
+
+    println("v3: " + v3)
+    println("v4: " + v4)
+    println("v5: " + v5)
+    println("v6: " + v6)
+    println("v7: " + v7)
+    println("v8: " + v8)
+
+    // default values
+//    val v3 = 4
+//    val v4 = 8
+//    val v5 = 64
+//    val v6 = 128
+//    val v7 = 128
+//    val v8 = 16
+
+    // generate kernel
+    val kernel = genMMKernel(v3, v4, v5, v6, v7, v8)
+
+    // create random values
+    val (at, b, gold) = randGold()
+
+    // run kernel
+    val result = runKernel(gen.OpenCLKernel(kernel), LocalSize((32, 8)), GlobalSize((M/4, N/8)), at, b)
+
+    // check result
+    util.assertSame(result._1, gold, "result is different from gold")
+
+    println(result._2)
+
+    Executor.shutdown()
+  }
+
+}
