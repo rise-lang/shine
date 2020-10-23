c97d9dd3aab45d519b944c9be80fb276480b1332
Johannes Lenfers <j.le@wwu.de>
2020-10-23 18:53:21 +0200

    fixed bug in output format

--- src/main/scala/apps/mmTuning.scala
+++ src/main/scala/apps/mmTuning.scala
@@ -12,6 +12,10 @@ object mmTuning {
   private val M = 128
   private val O = 128
 
+//  private val N = 1024
+//  private val M = 1024
+//  private val O = 1024
+
   private def randGold(): (Array[Array[Float]], Array[Array[Float]], Array[Float]) = {
     val rand = new scala.util.Random
     val At = Array.fill(O, N)(rand.nextFloat() * 10)
@@ -32,13 +36,13 @@ object mmTuning {
     val v6 = args(3).toInt
     val v7 = args(4).toInt
     val v8 = args(5).toInt
-
-    println("v3: " + v3)
-    println("v4: " + v4)
-    println("v5: " + v5)
-    println("v6: " + v6)
-    println("v7: " + v7)
-    println("v8: " + v8)
+//
+//    println("v3: " + v3)
+//    println("v4: " + v4)
+//    println("v5: " + v5)
+//    println("v6: " + v6)
+//    println("v7: " + v7)
+//    println("v8: " + v8)
 
     // default values
 //    val v3 = 4
@@ -55,14 +59,23 @@ object mmTuning {
     val (at, b, gold) = randGold()
 
     // run kernel
-    val result = runKernel(gen.OpenCLKernel(kernel), LocalSize((32, 8)), GlobalSize((M/4, N/8)), at, b)
+    try{
+//      println("gen kernel")
+//      val code = gen.OpenCLKernel(kernel)
+//      println("execute kernel")
+//      val result = runKernel(code, LocalSize((32, 8)), GlobalSize((M/4, N/8)), at, b)
+      val result = runKernel(gen.OpenCLKernel(kernel), LocalSize((32, 8)), GlobalSize((M/4, N/8)), at, b)
+      Executor.shutdown()
 
-    // check result
-    util.assertSame(result._1, gold, "result is different from gold")
-
-    println(result._2)
-
-    Executor.shutdown()
+      // check result
+      val testSame = util.assertSame(result._1, gold, "result is different from gold")
+      println(result._2.value)
+      sys.exit(0)
+    }catch{
+      case e:Throwable => {
+        Executor.shutdown()
+        sys.exit(1)
+      }
+    }
   }
-
 }
