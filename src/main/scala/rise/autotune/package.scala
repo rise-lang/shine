8374d0d96e5179f9a707269b76c333546d1d20c7
Thomas Koehler <basta.t.k+git@gmail.com>
2021-01-26 16:53:03 +0000

    initial autotuning API

---
+++ src/main/scala/rise/autotune/package.scala
@@ -0,0 +1,18 @@
+package rise
+
+import rise.core.Expr
+import rise.core.types.NatIdentifier
+import util.{Time, TimeSpan}
+
+package object autotune {
+  type Parameters = Seq[NatIdentifier]
+  case class Sample(p: Parameters, runtime: Option[TimeSpan[Time.ms]], timestamp: Int)
+
+  def search(e: Expr): Seq[Sample] = ???
+
+  private def collectParameters(e: Expr): Parameters = ???
+
+  private def generateJSON(p: Parameters): String = ???
+
+  def applyBest(e: Expr, samples: Seq[Sample]): Expr = ???
+}
