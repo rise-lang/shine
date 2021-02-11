5f81b9dfe0b7f71832beceb09cd177000be49ca0
Uma Zalakain <ping@umazalakain.info>
2021-02-11 14:27:25 +0000

    Move monad trait into utils

--- src/main/scala/rise/core/traverse.scala
+++ src/main/scala/rise/core/traverse.scala
@@ -1,23 +1,17 @@
 package rise.core
 
-import scala.language.implicitConversions
-import arithexpr.arithmetic.NamedVar
+import util.monad
 import rise.core.semantics._
 import rise.core.types._
+import scala.language.implicitConversions
 
 object traverse {
-  trait Monad[M[_]] {
-    def return_[T] : T => M[T]
-    def bind[T,S] : M[T] => (T => M[S]) => M[S]
-    def traverse[A] : Seq[M[A]] => M[Seq[A]] =
-      _.foldRight(return_(Nil : Seq[A]))({case (mx, mxs) =>
-        bind(mx)(x => bind(mxs)(xs => return_(x +: xs)))})
-  }
-
-  implicit def monadicSyntax[M[_], A](m: M[A])(implicit tc: Monad[M]) = new {
-    def map[B](f: A => B): M[B] = tc.bind(m)(a => tc.return_(f(a)) )
-    def flatMap[B](f: A => M[B]): M[B] = tc.bind(m)(f)
-  }
+  // Reexport util.monad.*
+  type Monad[M[_]] = monad.Monad[M]
+  type Pure[T] = monad.Pure[T]
+  implicit def monadicSyntax[M[_], A](m: M[A])(implicit tc: monad.Monad[M]) = monad.monadicSyntax(m)(tc)
+  val PureMonad = monad.PureMonad
+  val OptionMonad = monad.OptionMonad
 
   sealed trait VarType
   case object Binding extends VarType
@@ -181,18 +175,6 @@ object traverse {
 
   trait ExprTraversal[M[_]] extends Traversal[M] {
     override def `type`[T <: Type] : T => M[T] = return_
-  }
-
-  case class Pure[T](unwrap : T)
-  implicit object PureMonad extends Monad[Pure] {
-    override def return_[T] : T => Pure[T] = t => Pure(t)
-    override def bind[T,S] : Pure[T] => (T => Pure[S]) => Pure[S] =
-      v => f => v match { case Pure(v) => f(v) }
-  }
-
-  implicit object OptionMonad extends Monad[Option] {
-    def return_[T]: T => Option[T] = Some(_)
-    def bind[T, S]: Option[T] => (T => Option[S]) => Option[S] = v => v.flatMap
   }
 
   trait PureTraversal extends Traversal[Pure] { override def monad = PureMonad }
