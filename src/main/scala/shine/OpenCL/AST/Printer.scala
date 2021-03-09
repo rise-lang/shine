ca5f7ed36bbdb5f0b25b61a5eda8d6854a3bcb87
Lukas Siefke <lukassiefkehh@gmail.com>
2021-03-09 16:54:27 +0100

    rechange .print -> .name

--- src/main/scala/shine/OpenCL/AST/Printer.scala
+++ src/main/scala/shine/OpenCL/AST/Printer.scala
@@ -73,7 +73,7 @@ class Printer extends shine.C.AST.CPrinter {
   def printParamDecl(p: ParamDecl): Unit = {
     if (p.t.const) print("const ")
     p.t match {
-      case b: BasicType => print(s"${b.print} ${p.name}")
+      case b: BasicType => print(s"${b.name} ${p.name}")
       case s: StructType => print(s"struct ${s.name} ${p.name}")
       case _: UnionType => ???
       case _: ArrayType => throw new Exception("Arrays as parameters are not supported")
