c396ac7c80f3c2e79adabc5a490b2e56fa5b6f51
Uma Zalakain <ping@umazalakain.info>
2021-04-04 14:22:42 +0100

    Start working on dependent types

--- src/main/scala/rise/core/bidirectional.scala
+++ src/main/scala/rise/core/bidirectional.scala
@@ -1,7 +1,6 @@
 package rise.core
 
 import scala.collection.mutable.ListBuffer
-
 import rise.core.semantics._
 import rise.core.types._
 
@@ -21,40 +20,39 @@ object bidirectional {
   def error(msg: String)(implicit trace: Trace): Nothing = throw InferenceException(msg, trace)
 
   type TermCtx = Map[Identifier, Type]
-  type TypeCtx = Solution // FIXME: types should be TypeIdentifier => Type
 
-  def checkKinded[K <: Kind](typeCtx : TypeCtx, termCtx : TermCtx, expr : K#T, `type` : K#I)(implicit trace : Trace) : TypeCtx =
-    expr match {
-      case e : Expr => check(typeCtx, termCtx, e, `type`.asInstanceOf[Type])
-    }
-
-  def unify(a : Type, b : Type)(implicit trace : Trace) : TypeCtx =
+  def unify(a : Type, b : Type)(implicit trace : Trace) : Solution =
     Constraint.solve(Seq(TypeConstraint(a, b)), Seq())(Flags.ExplicitDependence.Off)
 
-  def check(typeCtx : TypeCtx, termCtx : TermCtx, expr : Expr, `type` : Type)(implicit trace : Trace) : TypeCtx = {
-    trace += s"check $typeCtx ; $termCtx |- $expr : ${`type`}"
+  def checkKind[K <: Kind](termCtx : TermCtx, expr : K#T, `type` : K#I)(implicit trace : Trace) : Solution = {
+    `type` match {
+      case t : TypeIdentifier => Solution.subs(`type`.asInstanceOf[Type], t)
+      case t : Nat => Solution.subs(`type`.asInstanceOf[NatIdentifier], t)
+    }
+  }
+
+  def check(termCtx : TermCtx, expr : Expr, `type` : Type)(implicit trace : Trace) : Solution = {
+    trace += s"check $termCtx |- $expr : ${`type`}"
 
     expr match {
       case Lambda(x, e) =>
         `type` match {
-          case FunType(a, b) => check(typeCtx, termCtx ++ Map(x -> a), e, b)
+          case FunType(a, b) => check(termCtx ++ Map(x -> a), e, b)
           case _ => error(s"${`type`} is not a function type")
         }
       case DepLambda(x, e) =>
         `type` match {
-          case DepFunType(x, t) => check(typeCtx ++ ???, termCtx, e, t)
+          case DepFunType(x, t) => check(termCtx, e, t)
           case _ => error(s"${`type`} is not a dependent function type")
         }
       case Literal(d) => unify(d.dataType, `type`)
       case p: Primitive => unify(p.typeScheme, `type`)
-      case _ =>
-        val t = infer(typeCtx, termCtx, expr)
-        unify(t, `type`)
+      case _ => unify(infer(termCtx, expr), `type`)
     }
   }
 
-  def infer(typeCtx : TypeCtx, termCtx : TermCtx, expr : Expr)(implicit trace : Trace) : Type = {
-    trace += s"infer $typeCtx ; $termCtx |- $expr"
+  def infer(termCtx : TermCtx, expr : Expr)(implicit trace : Trace) : Type = {
+    trace += s"infer $termCtx |- $expr"
 
     expr match {
       case x: Identifier =>
@@ -64,29 +62,32 @@ object bidirectional {
         }
       case Lambda(x, e) =>
         val xT = TypeIdentifier(freshName("x"))
-        val termCtx1 = termCtx ++ Map(x -> xT)
-        val eT = infer(typeCtx, termCtx1, e)
+        val eT = infer(termCtx ++ Map(x -> xT), e)
         FunType(xT, eT)
-      case DepLambda(x, e) => ???
+      case DepLambda(x, e) => x match {
+        case x : NatIdentifier =>
+          val eT = infer(termCtx, e)
+          DepFunType[NatKind, Type](x, eT)
+      }
       case App(f, e) =>
-        infer(typeCtx, termCtx, f) match {
+        infer(termCtx, f) match {
           case FunType(a, b) =>
-            val typeCtx1 = check(typeCtx, termCtx, e, a)
+            val typeCtx1 = check(termCtx, e, a)
             typeCtx1(b)
-          case _ => error(s"$typeCtx ; $termCtx |- $f is not a function type")
+          case _ => error(s"$termCtx |- $f is not a function type")
         }
       case DepApp(f, x) =>
-        infer(typeCtx, termCtx, f) match {
+        infer(termCtx, f) match {
           case DepFunType(xT, eT) =>
-            val typeCtx1 = checkKinded(typeCtx, termCtx, x, xT)
+            val typeCtx1 = checkKind(termCtx, x, xT)
             typeCtx1(eT)
-          case _ => error(s"$typeCtx ; $termCtx |- $f is not a dependent function type")
+          case _ => error(s"$termCtx |- $f is not a dependent function type")
         }
       case TypeAnnotation(e, t) =>
-        val typeCtx1 = check(typeCtx, termCtx, e, t)
+        val typeCtx1 = check(termCtx, e, t)
         typeCtx1(t)
       case TypeAssertion(e, t) =>
-        val typeCtx1 = check(typeCtx, termCtx, e, t)
+        val typeCtx1 = check(termCtx, e, t)
         typeCtx1(t)
       case Literal(d) => d.dataType
       case p : Primitive => p.typeScheme
@@ -95,7 +96,11 @@ object bidirectional {
 
   implicit val trace : Trace = new Trace()
   val x = Identifier(freshName("x"))(TypePlaceholder)
-  val example : Expr = App(TypeAnnotation(Lambda(x, x)(TypePlaceholder), FunType(int, int)), Literal(IntData(1)))(TypePlaceholder)
+  val n = NatIdentifier(freshName("x"))
+  val example0 : Expr = App(TypeAnnotation(Lambda(x, x)(TypePlaceholder), FunType(int, int)), Literal(IntData(1)))(TypePlaceholder)
   val example1 : Expr = Lambda(x, x)(TypePlaceholder)
   val example2 : Expr = App(Lambda(x, x)(TypePlaceholder) , Literal(IntData(1)))(TypePlaceholder)
+  val example3 : Expr = DepLambda[NatKind](n, Literal(NatData(n)))(TypePlaceholder)
+  val example4 : Expr = DepApp[NatKind](example3, 2)(TypePlaceholder)
+  val example5 : Expr = DepApp[TypeKind](example3, bool)(TypePlaceholder)
 }
