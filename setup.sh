c99a19baa12075a0e6037dda6105006db01fbd8a
Johannes Lenfers <j.le@wwu.de>
2021-04-24 12:43:36 +0200

    added commands to build clap library

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 git submodule update --init --recursive
 git submodule status
+
+# build clap library 
+mkdir -p lib/clap/build
+cmake -DTRACK_EVENTS=ON -B lib/clap/build lib/clap
+make -C lib/clap/build -j8
