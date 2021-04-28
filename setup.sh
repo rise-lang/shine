2b5bcc7f89ccfb04c392485eeeb7b0065b128371
Johannes Lenfers <j.le@wwu.de>
2021-04-28 14:37:28 +0200

    removed clap build instructions from setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,3 @@
 git submodule update --init --recursive
 git submodule status
 
-# build clap library 
-mkdir -p lib/clap/build
-cmake -DTRACK_EVENTS=ON -B lib/clap/build lib/clap
-make -C lib/clap/build -j8
