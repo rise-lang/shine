d3ed9c84d81621b2f9b15b23d6c2780bc2c872a8
Michel Steuwer <michel.steuwer@ed.ac.uk>
2021-05-03 16:27:50 +0100

    Removed macros

--- src/main/scala/shine/DPIA/primitives/imperative/MkDPairSndAcc.scala
+++ src/main/scala/shine/DPIA/primitives/imperative/MkDPairSndAcc.scala
@@ -3,11 +3,12 @@ package shine.DPIA.primitives.imperative
 import shine.DPIA.NatIdentifier
 import shine.DPIA.Phrases._
 import shine.DPIA.Types.{AccType, DataType}
-import shine.macros.Primitive.accPrimitive
 
-@accPrimitive
 final case class MkDPairSndAcc(fst: NatIdentifier,
                                sndT: DataType,
                                A: Phrase[AccType]) extends AccPrimitive {
   override val t = AccType(sndT)
+
+  override def visitAndRebuild(v: VisitAndRebuild.Visitor): Phrase[AccType] =
+    MkDPairSndAcc(v.nat(fst), v.data(sndT), VisitAndRebuild(A, v))
 }
