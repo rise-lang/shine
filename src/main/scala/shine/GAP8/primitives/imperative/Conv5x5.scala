03ba5fd126f5cb2c4ba90968d99903a252d6e843
bpervan <branimir.pervan@fer.hr>
2021-07-26 13:29:13 +0200

    Swapped dimensions of the input and output arrays

--- src/main/scala/shine/GAP8/primitives/imperative/Conv5x5.scala
+++ src/main/scala/shine/GAP8/primitives/imperative/Conv5x5.scala
@@ -10,9 +10,9 @@ import shine.DPIA.Types.Kind.{ Identifier => _, _ }
 import shine.DPIA._
 final case class Conv5x5(val w: Nat, val h: Nat, val bias: Nat, val dt: DataType, val in: Phrase[ExpType], val filter: Phrase[ExpType], val out: Phrase[AccType]) extends CommandPrimitive {
   assert {
-    in :: expT(ArrayType(w, ArrayType(h, dt)), read)
+    in :: expT(ArrayType(h, ArrayType(w, dt)), read)
     filter :: expT(ArrayType(26, dt), read)
-    out :: accT(ArrayType(w - 4, ArrayType(h - 4, dt)))
+    out :: accT(ArrayType(h - 4, ArrayType(w - 4, dt)))
     true
   }
   override val t: CommType = comm
--- src/main/scala/shine/GAP8/primitives/imperative/Conv7x4.scala
+++ src/main/scala/shine/GAP8/primitives/imperative/Conv7x4.scala
@@ -10,9 +10,9 @@ import shine.DPIA.Types.Kind.{ Identifier => _, _ }
 import shine.DPIA._
 final case class Conv7x4(val w: Nat, val h: Nat, val bias: Nat, val dt: DataType, val in: Phrase[ExpType], val filter: Phrase[ExpType], val out: Phrase[AccType]) extends CommandPrimitive {
   assert {
-    in :: expT(ArrayType(w, ArrayType(h, dt)), read)
+    in :: expT(ArrayType(h, ArrayType(w, dt)), read)
     filter :: expT(ArrayType(28, dt), read)
-    out :: accT(ArrayType(w - 6, ArrayType(h - 3, dt)))
+    out :: accT(ArrayType(h - 6, ArrayType(w - 3, dt)))
     true
   }
   override val t: CommType = comm
