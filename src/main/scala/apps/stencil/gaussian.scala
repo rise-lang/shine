d7ab870e82903e02f9dd7216d8a6a28fcf0f5577
Thomas Koehler <basta.t.k+git@gmail.com>
2021-09-02 11:39:58 +0100

    gaussian WIP

--- src/main/scala/apps/stencil/gaussian.scala
+++ src/main/scala/apps/stencil/gaussian.scala
@@ -68,4 +68,48 @@ object gaussian {
     })) >> padClamp2D(2, 2) >> scatterShift(2) >> map(scatterShift(2))
     // TODO: map(padEmpty(4)) >> padEmpty(4) instead ?
   ))
+
+  val v__0 = 1
+  val v__1 = 2
+  val gaussianKeplerBest: ToBeTyped[Expr] = depFun((n: Nat, m: Nat) => fun(
+    (n `.` m `.` f32) ->: (n `.` m `.` f32)
+  )(input => input |>
+    slide2D(4+v__0, v__0, 4+v__1, v__1) >>
+    slide2D(5, 1) >> mapGlobal(1)(mapGlobal(0)(mapSeq(mapSeq(fun { nbh =>
+      def at(i: Int, j: Int): ToBeTyped[Expr] =
+        nbh `@` lidx(i, 5) `@` lidx(j, 5)
+      val nnww = at(0, 0)
+      val nnw  = at(0, 1)
+      val nn   = at(0, 2)
+      val nne  = at(0, 3)
+      val nnee = at(0, 4)
+      val nww  = at(1, 0)
+      val nw   = at(1, 1)
+      val n    = at(1, 2)
+      val ne   = at(1, 3)
+      val nee  = at(1, 4)
+      val ww   = at(2, 0)
+      val w    = at(2, 1)
+      val c    = at(2, 2)
+      val e    = at(2, 3)
+      val ee   = at(2, 4)
+      val sww  = at(3, 0)
+      val sw   = at(3, 1)
+      val s    = at(3, 2)
+      val se   = at(3, 3)
+      val see  = at(3, 4)
+      val ssww = at(4, 0)
+      val ssw  = at(4, 1)
+      val ss   = at(4, 2)
+      val sse  = at(4, 3)
+      val ssee = at(4, 4)
+
+      jacobi(nnww)(nnw)(nn)(nne)(nnee)(
+        nww)(nw)(n)(ne)(nee)(
+        ww)(w)(c)(e)(ee)(
+        sww)(sw)(s)(se)(see)(
+        ssww)(ssw)(ss)(sse)(ssee)
+    })))) >> padClamp2D(2, 2) >> scatterShift(2) >> map(scatterShift(2))
+    // TODO: map(padEmpty(4)) >> padEmpty(4) instead ?
+  ))
 }
