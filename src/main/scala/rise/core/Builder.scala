12d63cb7e711230399cc61c0a571f85bd106c199
Thomas Koehler <basta.t.k+git@gmail.com>
2021-09-10 11:34:11 +0100

    remove unnecessary toBeTyped call in Builder

--- src/main/scala/rise/core/Builder.scala
+++ src/main/scala/rise/core/Builder.scala
@@ -8,11 +8,11 @@ trait Builder {
   def apply(e: DSL.ToBeTyped[Expr]): DSL.ToBeTyped[App] =
     DSL.app(apply, e)
   def apply(n: Nat): DSL.ToBeTyped[DepApp[Nat]] =
-    DSL.depApp(NatKind, DSL.toBeTyped(apply), n)
+    DSL.depApp(NatKind, apply, n)
   def apply(dt: DataType): DSL.ToBeTyped[DepApp[DataType]] =
-    DSL.depApp(DataKind, DSL.toBeTyped(apply), dt)
+    DSL.depApp(DataKind, apply, dt)
   def apply(a: AddressSpace): DSL.ToBeTyped[DepApp[AddressSpace]] =
-    DSL.depApp(AddressSpaceKind, DSL.toBeTyped(apply), a)
+    DSL.depApp(AddressSpaceKind, apply, a)
 
   def unapply(arg: Expr): Boolean =
     throw new Exception("unapply method must be overridden")
