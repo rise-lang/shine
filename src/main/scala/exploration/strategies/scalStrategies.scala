f93efab892e77561bd1c3812a4cb46a690fb5189
Johannes Lenfers <j.le@wwu.de>
2021-09-26 16:42:04 +0200

    updated lowering strategies of mv exploration

--- src/main/scala/exploration/strategies/scalStrategies.scala
+++ src/main/scala/exploration/strategies/scalStrategies.scala
@@ -3,32 +3,37 @@ import elevate.core.Strategy
 import elevate.core.strategies.traversal.{allTopdown, bottomUp, somebu, topDown}
 import rise.elevate.{Rise, tunable}
 import rise.elevate.rules.algorithmic.{fuseReduceMap, splitJoin}
-import rise.elevate.rules.lowering.addRequiredCopies
+import rise.elevate.rules.lowering.{addRequiredCopies, reduceOCL}
 import rise.elevate.rules.traversal.default.RiseTraversable
 
 object scalStrategies {
 
+  val lowerAll = addRequiredCopies() `;` rise.elevate.rules.lowering.specializeSeq() `;` reduceOCL()
+
   // define lowering strategy
   // topdown map - mapGlb
-  def lowerGs =
-    topDown(rise.elevate.rules.lowering.mapGlobal(0)) `;` addRequiredCopies() `;` rise.elevate.rules.lowering.specializeSeq()
+  def lowerGs = {
+    topDown(rise.elevate.rules.lowering.mapGlobal(0)) `;`
+      lowerAll
+  }
 
   def lowerGsGs =
     topDown(rise.elevate.rules.lowering.mapGlobal(0)) `;`
       topDown(rise.elevate.rules.lowering.mapGlobal(1)) `;`
-      addRequiredCopies() `;` rise.elevate.rules.lowering.specializeSeq()
+      lowerAll
+
 
   def lowerWrgLcl =
     topDown(rise.elevate.rules.lowering.mapWorkGroup(0)) `;`
       topDown(rise.elevate.rules.lowering.mapLocal(0)) `;`
-      addRequiredCopies() `;` rise.elevate.rules.lowering.specializeSeq()
+      lowerAll
 
   def lowerWrgWrgLclLcl =
     topDown(rise.elevate.rules.lowering.mapWorkGroup(0)) `;`
       topDown(rise.elevate.rules.lowering.mapWorkGroup(1)) `;`
       topDown(rise.elevate.rules.lowering.mapLocal(0)) `;`
       topDown(rise.elevate.rules.lowering.mapLocal(1)) `;`
-      addRequiredCopies() `;` rise.elevate.rules.lowering.specializeSeq()
+      lowerAll
 
   // add strategies
   val sjtp = bottomUp(tunable(splitJoin))(RiseTraversable)
