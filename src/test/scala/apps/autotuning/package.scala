2c020bf0adb2a07204e9a18b8c4e3b9530a0647e
Johannes Lenfers <j.le@wwu.de>
2022-06-27 17:42:09 +0200

    added option to print and execute user-configs for auto tuning experiments£

--- src/test/scala/apps/autotuning/package.scala
+++ src/test/scala/apps/autotuning/package.scala
@@ -1,5 +1,6 @@
 package apps
 
+import exploration.explorationUtil.jsonParser.readFile
 import rise.autotune
 import rise.autotune.{AutoTuningError, HostCode, Median, Minimum, Timeouts, Tuner}
 import rise.core.Expr
@@ -22,16 +23,46 @@ package object autotuning {
                      strategyMode: Option[(Expr, Map[String, Int], Map[String, List[Int]]) => Either[String, Expr]] = None, // enable strategy mode
                      executor: Option[Expr => (Either[AutoTuningError, Double], Option[Double], Option[Double], Option[Double])] = None, // todo change this to exeuction result
                      plotOnly: Boolean = false,
-                     disableChecking: Boolean = false
+                     disableChecking: Boolean = false,
+                     expert: Option[Map[Nat, Nat]] = None,
+                     default: Option[Map[Nat, Nat]] = None
                    ) = {
 
     plotOnly match {
       case true =>
         os.isDir(os.Path(output, os.pwd)) match {
-          case true => plotExperiment(name, configFiles, output)
+          case true => {
+
+            // if expert -> read from output
+            val expertConfiguration = expert match {
+              case Some(_) => readConfig(output + "/manual_configs/" + "expert.csv") // read in from output
+              case None => None // do nothing
+            }
+
+            // if default -> read from output
+            val defaultConfiguration = default match {
+              case Some(_) => readConfig(output + "/manual_configs/" + "default.csv") // read in from output
+              case None => None // do nothing
+            }
+
+            plotExperiment(name, configFiles, output, expertConfiguration, defaultConfiguration)
+          }
           case false => println("experiment's output does not exist - ignore plotting")
         }
       case false =>
+
+        // if expert -> read from output
+        val expertConfiguration = expert match {
+          case Some(config) => runConfig(config, e, hostCode, output, "expert")
+          case None => None // do nothing
+        }
+
+        // if default -> read from output
+        val defaultConfiguration = default match {
+          case Some(config) => runConfig(config, e, hostCode, output, "default")
+          case None => None // do nothing
+        }
+
         // run tuning
         for (i <- 1 to iterations) {
           configFiles.foreach(configFile =>
@@ -42,10 +73,67 @@ package object autotuning {
             }
           )
 
-          // plot experiments after each iteration of all configs
-          plotExperiment(name, configFiles, output)
+          configFiles.size match {
+            case 0 => // ignore
+            case _ =>
+              // plot experiments after each iteration of all configs
+              plotExperiment(name, configFiles, output, expertConfiguration, defaultConfiguration)
+          }
         }
     }
+  }
+
+  // read config values from output
+  def readConfig(
+                  filePath: String
+                ): Option[Double] = {
+
+    val file = new File(filePath)
+    val output = if (file.exists()) {
+      Some(readFile(filePath).toDouble)
+    } else {
+      None
+    }
+
+    output
+  }
+
+  def runConfig(
+                 config: Map[Nat, Nat],
+                 e: Expr,
+                 hostCode: HostCode,
+                 output: String,
+                 file: String
+               ): Option[Double] = {
+
+    val eReplaced = rise.core.substitute.natsInExpr(config, e)
+
+    // todo merge this information and information from runTuning
+    val result = autotune.execution.execute(
+      expression = eReplaced,
+      hostCode = hostCode,
+      timeouts = Timeouts(1000, 10000, 10000),
+      executionIterations = 100,
+      speedupFactor = 100,
+      execution = Median
+    )
+    println("result: " + result.runtime)
+
+    val configResult = result.runtime match {
+      case Left(_) => None
+      case Right(value) => Some(value.value)
+    }
+
+    // create output directory
+    (s"mkdir -p ${output}/manual_configs" !!)
+
+    // write result into file
+    val path = output + "/manual_configs/" + file + ".csv"
+    util.writeToPath(path, configResult.get.toString)
+
+    // todo print config as well
+
+    configResult
   }
 
   def runTuning(
@@ -82,7 +170,13 @@ package object autotuning {
     autotune.search(tuner)(e)
   }
 
-  def plotExperiment(name: String, configs: Seq[String], output: String) = {
+  def plotExperiment(
+                      name: String,
+                      configs: Seq[String],
+                      output: String,
+                      expertConfiguration: Option[Double] = None,
+                      defaultConfiguration: Option[Double] = None
+                    ) = {
     try {
 
 
@@ -105,17 +199,56 @@ package object autotuning {
       val outputScript = s"${name}"
 
       // plot
-      val command: (String, String, String) => String = (config, folders, output) =>
-        "hm-plot-optimization-results " +
-          s"-j ${config} " +
-          "-i " +
-          folders +
-          "-l " +
-          names +
-          s"-o ${output}.pdf " +
-          "-log " +
-          "--y_label \"Log Runtime(ms)\" " +
-          s"--title ${name} "
+      val commands: (String, String, String, Option[Double], Option[Double]) => Seq[String] = (config, folders, output, expertConfiguration, defaultConfiguration) => {
+
+        val command: String =
+          "hm-plot-optimization-results " +
+            s"-j ${config} " +
+            "-i " +
+            folders +
+            "-l " +
+            names +
+            s"-o ${output}.pdf " +
+            "-log " +
+            "--y_label \"Log Runtime(ms)\" " +
+            s"--title ${name} "
+
+        val commandExp: String = expertConfiguration match {
+          case Some(value) =>
+            "hm-plot-optimization-results " +
+              s"-j ${config} " +
+              "-i " +
+              folders +
+              "-l " +
+              names +
+              s"-o ${output}_expert.pdf " +
+              "-log " +
+              "--y_label \"Log Runtime(ms)\" " +
+              s"--exp ${value} " +
+              s"--title ${name} "
+
+          case None => ""
+
+        }
+
+        val commandDefault: String = defaultConfiguration match {
+          case Some(value) =>
+            "hm-plot-optimization-results " +
+              s"-j ${config} " +
+              "-i " +
+              folders +
+              "-l " +
+              names +
+              s"-o ${output}_default.pdf " +
+              "-log " +
+              "--y_label \"Log Runtime(ms)\" " +
+              s"--exp ${value} " +
+              s"--title ${name} "
+          case None => ""
+        }
+
+        Seq(command, commandExp, commandDefault)
+      }
 
       // create unique filepath
       val path = output + "/" + "plot_hm.sh"
@@ -129,16 +262,17 @@ package object autotuning {
 
       // write plotting script
       val header = "#!/bin/bash\n"
-      val content = header + command(configScript, foldersScript, outputScript)
+      val content = header + commands(configScript, foldersScript, outputScript, expertConfiguration, defaultConfiguration).mkString("\n")
       util.writeToPath(uniqueFilepath, content)
 
-      println("command: \n" + command(configPrinting, foldersPrinting, outputPrinting))
+      println("command: \n" + commands(configPrinting, foldersPrinting, outputPrinting, expertConfiguration, defaultConfiguration))
 
-      command(configPrinting, foldersPrinting, outputPrinting) !!
+      commands(configPrinting, foldersPrinting, outputPrinting, expertConfiguration, defaultConfiguration).foreach(command => {
+        command !!
+      })
+
     } catch {
       case e: Throwable => println("printing of experiment failed")
     }
   }
-
-
 }
