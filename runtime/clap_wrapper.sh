d05dd022f63482eaaeba1965e44c3ec44086fc1f
Johannes Lenfers <j.le@wwu.de>
2023-02-17 21:24:46 +0100

    adjusted clap wrapper for linux

--- runtime/clap_wrapper.sh
+++ runtime/clap_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#LD_PRELOAD=./lib/clap/build/libClap.so "$1" "$2"
+LD_PRELOAD=./lib/clap/build/libClap.so "$1" "$2"
 
-LD_PRELOAD=./lib/clap/build/libClap.dylib "$1" "$2"
+#LD_PRELOAD=./lib/clap/build/libClap.dylib "$1" "$2"
 
