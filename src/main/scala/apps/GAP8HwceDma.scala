b8423cddb8f18b3b0238fa8ca990f5ea69334205
bpervan <branimir.pervan@fer.hr>
2023-05-04 14:48:47 +0200

    Added GAP8 + HWCE + DMA testing application

--- src/main/scala/apps/GAP8HwceDma.scala
+++ src/main/scala/apps/GAP8HwceDma.scala
@@ -1,26 +1,105 @@
 package apps
 
 import rise.GAP8.DSL.gap8Run
-import rise.GAP8.primitives.{copyToL1, copyToL2}
+import rise.GAP8.primitives.{allocL1, copyToL1, copyToL2, gap8hwConv3x3}
 import rise.core.DSL.Type.TypeConstructors
-import rise.core.DSL.{ToBeTyped, fun}
-import rise.core.primitives.mapSeq
-import rise.core.types.DataType.{ArrayType, u8}
+import rise.core.DSL.Type.`.`
+import rise.core.DSL.{ToBeTyped, depFun, fun, letf}
+import rise.core.primitives.{add, mapSeq}
+import rise.core.types.DataType.{ArrayType, i16, u8}
+import rise.core.types.Nat
 import rise.elevate.Rise
 import shine.GAP8.Module.translateToString
 
 object GAP8HwceDma {
   def main(args: Array[String]): Unit = {
-    val expr: ToBeTyped[Rise] = fun(ArrayType(10, u8) ->: ArrayType(10, u8))(in =>
+    val size: Nat = 10
+    val fixSize: ToBeTyped[Rise] = fun(ArrayType(size, u8) ->: ArrayType(size, u8))(in =>
+        gap8Run(8)(
+          in |>
+            copyToL1 |>
+            allocL1 |>
+            mapSeq(fun(x => x)) |>
+            allocL1 |>
+            copyToL2
+        )
+      )
+    println(translateToString(util.gen.gap8.hosted.fromExpr(fixSize)))
+
+    val varSize: ToBeTyped[Rise] = depFun((sz: Nat) =>
+      fun(ArrayType(sz, u8) ->: ArrayType(sz, u8))(in =>
+        gap8Run(8)(
+          in |>
+            copyToL1 |>
+            allocL1 |>
+            mapSeq(fun(x => x)) |>
+            allocL1 |>
+            copyToL2
+        )))
+    //println(translateToString(util.gen.gap8.hosted.fromExpr(varSize)))
+
+    val hwceDebug: ToBeTyped[Rise] = fun(
+      ArrayType(size, ArrayType(size, u8)) ->:
+        ArrayType(3, ArrayType(3, u8)) ->:
+        ArrayType(size - 2, ArrayType(size - 2, u8)))((in, filter) =>
       gap8Run(8)(
-        in |>
-          mapSeq(fun(x => x)) |>
-          copyToL1 |>
-          mapSeq(fun(x => x)) |>
-          copyToL2
+        gap8hwConv3x3(0)(in, filter)
       )
     )
+    println(translateToString(util.gen.gap8.hosted.fromExpr(hwceDebug)))
 
-    println(translateToString(util.gen.gap8.hosted.fromExpr(expr)))
+
+    val fixSizeDmaHwce: ToBeTyped[Rise] = fun(
+      ArrayType(size, ArrayType(size, u8)) ->:
+        ArrayType(3, ArrayType(3, u8)) ->:
+        ArrayType(size - 2, ArrayType(size - 2, u8)))((in, filter) =>
+      gap8Run(8)(
+        in |>
+          copyToL1 |>
+          allocL1 |>
+          letf(innerin =>
+            filter |>
+              copyToL1 |>
+              allocL1 |> letf(innerf =>
+                gap8hwConv3x3(0)(innerin, innerf) |> allocL1 |> copyToL2
+            )
+          )
+      )
+    )
+    println(translateToString(util.gen.gap8.hosted.fromExpr(fixSizeDmaHwce)))
+
+    /*val varSize: ToBeTyped[Rise] = depFun((n: Nat) =>
+      fun(ArrayType(n, u8) ->: ArrayType(n, u8))(in =>
+        gap8Run(8)(
+          in |>
+            copyToL1 |>
+            allocL1 |>
+            mapSeq(fun(x => x)) |>
+            allocL1 |>
+            copyToL2
+        )
+      )
+    )
+    println(translateToString(util.gen.gap8.hosted.fromExpr(varSize)))*/
+
+    /*
+    *
+    val w: Nat = 9
+    val h: Nat = 9
+    val twoDandHWCE: ToBeTyped[Rise] = {
+      fun(ArrayType(w, ArrayType(h, i16)) ->: ArrayType(3, ArrayType(3, i16)) ->: ArrayType((w-2), ArrayType((h-2), i16)))((in, filter) =>
+        gap8Run(8)(
+          in |>
+            copyToL1 |>
+            allocL1 |>
+            mapSeq(fun(x =>x)) |>
+            allocL1 |>
+            copyToL2
+          //gap8hwConv3x3(0)(in)(filter)
+        )
+      )
+    }
+    println(translateToString(util.gen.gap8.hosted.fromExpr(twoDandHWCE)))
+    * */
   }
 }
