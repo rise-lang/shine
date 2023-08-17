b49fad50b356aa78d94ee3d4a30d28f824bf37b2
Johannes Lenfers <j.le@wwu.de>
2023-08-17 22:20:29 +0200

    added simple test case to test autotuning loop

---
+++ src/test/scala/apps/autotuning/testTuning.scala
@@ -0,0 +1,102 @@
+package apps.autotuning
+
+import arithexpr.arithmetic.RangeMul
+import rise.autotune
+import rise.autotune._
+import rise.core._
+import rise.core.types._
+import shine.OpenCL.{GlobalSize, LocalSize}
+
+class testTuning extends test_util.Tests {
+
+  val kmeans: Expr =
+    tuningParam("gs0", RangeMul(1, 1024, 2), (gs0: Nat) =>
+      tuningParam("gs1", RangeMul(1, 1024, 2), (gs1: Nat) =>
+        tuningParam("ls0", RangeMul(1, 1024, 2), (ls0: Nat) =>
+          tuningParam("ls1", RangeMul(1, 1024, 2), (ls1: Nat) =>
+            wrapOclRun(LocalSize(ls0, ls1), GlobalSize(gs0, gs1))(apps.kmeans.kmeansOcl)
+          ))))
+
+
+  // scalastyle:off
+  val init: (Int, Int, Int) => String = (p, c, f) => {
+    s"""
+       |  const int P = ${p};
+       |  const int C = ${c};
+       |  const int F = ${f};
+       |  srand(time(NULL));
+       |  Buffer features = createBuffer(ctx, F * P * sizeof(float), HOST_WRITE | DEVICE_READ);
+       |  Buffer clusters = createBuffer(ctx, C * F * sizeof(float), HOST_WRITE | DEVICE_READ);
+       |  Buffer output = createBuffer(ctx, P * sizeof(float), HOST_READ | DEVICE_WRITE);
+       |
+       |  float* in_features = hostBufferSync(ctx, features, F * P * sizeof(float), HOST_WRITE);
+       |  for (int i = 0; i < F * P ; i++) {
+       |      // in_features[i] = (float)(rand() % 100);
+       |      in_features[i] = (float)(i+1);
+       |  }
+       |
+       |  float* in_clusters = hostBufferSync(ctx, clusters, C * F * sizeof(float), HOST_WRITE);
+       |    for (int i = 0; i < F * P ; i++) {
+       |      // in_clusters[i] = (float)(rand() % 100);
+       |        in_features[i] = (float)(i+1);
+       |    }
+       |
+       |  deviceBufferSync(ctx, features, F * P * sizeof(float), DEVICE_READ);
+       |  deviceBufferSync(ctx, clusters, C * F * sizeof(float), DEVICE_READ);
+       |""".stripMargin
+  }
+  val compute =
+    s"""
+       |    fun_init_run(ctx, output, P, C, F, features, clusters);
+       |""".stripMargin
+
+  val finish =
+    s"""
+       |  // could add error checking
+       |  destroyBuffer(ctx, features);
+       |  destroyBuffer(ctx, clusters);
+       |  destroyBuffer(ctx, output);
+       |""".stripMargin
+  // scalastyle:on
+
+  test("run autotuning test based on kmeans") {
+
+    val inputSize: Int = 1 << 10
+
+    // expert configuration
+    val expertConfiguration: Map[Nat, Nat] = Map(
+      TuningParameter("ls0") -> (256: Nat),
+      TuningParameter("ls1") -> (1: Nat),
+      TuningParameter("gs0") -> (1024: Nat),
+      TuningParameter("gs1") -> (1: Nat),
+    )
+
+    val defaultConfiguration: Map[Nat, Nat] = Map(
+      TuningParameter("ls0") -> (1: Nat),
+      TuningParameter("ls1") -> (16: Nat),
+      TuningParameter("gs0") -> (1024: Nat),
+      TuningParameter("gs1") -> (1: Nat),
+    )
+
+    val configs = Seq(
+      s"autotuning/config/kmeans/${inputSize.toString}/rs_cot_${inputSize.toString}.json",
+      s"autotuning/config/kmeans/${inputSize.toString}/rs_emb_${inputSize.toString}.json",
+      s"autotuning/config/kmeans/${inputSize.toString}/bolog_cot_${inputSize.toString}.json",
+      s"autotuning/config/kmeans/${inputSize.toString}/atf_emb_${inputSize.toString}.json",
+      s"autotuning/config/kmeans/${inputSize.toString}/ytoptccs_${inputSize.toString}.json",
+    )
+
+    runExperiment(
+      name = s"kmeans_${inputSize}",
+      configFiles = configs,
+      iterations = 3,
+      output = s"experiments/autotuning/kmeans_${inputSize}",
+      e = kmeans,
+      hostCode = HostCode(init(inputSize, 10, 34), compute, finish),
+      inputSizes = Seq(inputSize, 10, 34),
+      plotOnly = false,
+      expert = Some(expertConfiguration),
+      default = Some(defaultConfiguration)
+    )
+  }
+}
