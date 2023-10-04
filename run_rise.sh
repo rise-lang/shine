c5a3acf850838e0f24402eea176c8cc738675d3e
Johannes Lenfers <j.le@wwu.de>
2023-10-04 02:32:19 +0200

    clean up

--- run_rise.sh
+++ run_rise.sh
@@ -8,9 +8,9 @@ else
 fi
 
 sbt "testOnly apps.autotuning.asumTuning"
-#sbt "testOnly apps.autotuning.harrisTuning"
-#sbt "testOnly apps.autotuning.kmeansTuning"
-#sbt "testOnly apps.autotuning.mmCPU"
-#sbt "testOnly apps.autotuning.mmTuning"
-#sbt "testOnly apps.autotuning.scalTuning"
-#sbt "testOnly apps.autotuning.stencil"
+sbt "testOnly apps.autotuning.harrisTuning"
+sbt "testOnly apps.autotuning.kmeansTuning"
+sbt "testOnly apps.autotuning.mmCPU"
+sbt "testOnly apps.autotuning.mmTuning"
+sbt "testOnly apps.autotuning.scalTuning"
+sbt "testOnly apps.autotuning.stencil"
